cb06eaa7918819ae68879403d53351ec54ddcd95
u <e@example.com>
2025-06-17 13:39:49 +0200

    index por mejorar

--- JS/app.js
+++ JS/app.js
@@ -1,2 +1,58 @@
+document.addEventListener('DOMContentLoaded',function(){
+    const mainContent = document.getElementById('main-content');
+    const buttons = document.querySelectorAll('nav button');
 
-//estoy empezando
+    buttons.forEach(button => {
+        button.addEventListener('click', function() {
+            const page = button.getAttribute('data-page');
+            loadPage(page);
+        });
+    });
+
+    //CARGA INICIAL
+    loadPage('home');
+
+    function loadPage(page){
+        let content = '';
+        switch(page){
+            case 'inicio': content = inicioContent;
+            break;
+            case 'rutas': content = rutasContent;
+            break;
+            case 'biblioteca': content = bibliotecaContent;
+            break;
+            case 'interrogante': content = interroganteContent;
+            break;
+            default: content = inicioContent;
+        }}
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
