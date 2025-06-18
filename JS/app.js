94f9d701f27e4d0711a2cf5e5fb31a65043c9f2f
u <e@example.com>
2025-06-18 13:58:41 +0200

    creacion de la segunda sección

--- JS/app.js
+++ JS/app.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded',function(){
+/*  document.addEventListener('DOMContentLoaded',function(){
     const mainContent = document.getElementById('main-content');
     const buttons = document.querySelectorAll('nav button');
 
@@ -10,8 +10,25 @@ document.addEventListener('DOMContentLoaded',function(){
     });
 
     //CARGA INICIAL
-    loadPage('home');
-
+    
+    const  inicioContent = ` 
+            <div class="pergamino">  
+                <h2 class="h">Relatos de Terror, Secretos del Alma <br> y <br> Misterios de Cataluña</h2>
+                <div class="contenido">
+                    <p>Bienvenido a Catuluña en la oscuridad, un espacio dedicado a explorar el lado oculto de nuestra tierra. <br><br>
+                    Aquí encontrarás tres empresas especializadas en rutas paranormales por Cataluña, listas para llevarte a escenarios reales de miedo,
+                    leyendas y fenómenos inexplicables.
+                    <br><br>Adéntrate también en nuestra pequeña biblioteca, una colección exclusiva de libros y relatos de terror escritos por autores catalanes.
+                    Cada historia es un eco del alma, un secreto enterrado en el tiempo, un susurro que cobra vida al caer la noche. <br><br>
+                    ¿Estás listo para adentarte en la oscuridad....?   </p>
+                </div>   
+            </div>`
+    
+    const rutasContent = "<p>Próximamente</p>";
+    const bibliotecaContent = "<p>En construcción</p>";
+    const interroganteContent = "<p>Secreto...</p>";
+    
+    
     function loadPage(page){
         let content = '';
         switch(page){
@@ -24,35 +41,13 @@ document.addEventListener('DOMContentLoaded',function(){
             case 'interrogante': content = interroganteContent;
             break;
             default: content = inicioContent;
-        }}
-
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-})
+        }
+        
+        mainContent.innerHTML = content
+        
+        
+    }
+    loadPage('inicio');
+   
+ 
+})*/
